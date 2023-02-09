088ff5731885f93421e90832640fb4250ea643cc
Suleyman Kutlu <suleyman.kutlu@actian.com>
2023-02-09 13:04:59 +0000

    working avalanche console test

--- charts/k6-synthetic-tests/synthetic-tests/avalanche.actiandatacloud.com.login.js
+++ charts/k6-synthetic-tests/synthetic-tests/avalanche.actiandatacloud.com.login.js
@@ -8,10 +8,10 @@ export const options = {
     AvalancheLoginProduction: {
       exec: 'avalanche_login_production',
       executor: 'shared-iterations',
-      gracefulStop: '30s',
+      gracefulStop: '60s',
       maxDuration: '10m',
-      vus: 1,
-      iterations: 1
+      vus: 3,
+      iterations: 3
     }
   }
 }
@@ -24,47 +24,70 @@ export function avalanche_login_production() {
   console.error('error')
 
   const browser = chromium.launch({
+    args: ['start-maximized'],
     headless: true,
     slowMo: '500ms' // slow down by 500ms
   });
   const context = browser.newContext();
   const page = context.newPage();
 
-  // Goto front page, find login link and click it
-
-  const resp = page.goto('https://avalanche.actiandatacloud.com/');
+  // Goto main page, waittill page is loaded and check HTTP status code
+  const resp = page.goto('https://avalanche.actiandatacloud.com/')
+  page.waitForLoadState()
   check(resp, {
     'ResponseCodeIs200': (resp) => resp.status() == 200,
-  });
-  page.$('input[id="username"]').type(__ENV.first_username);
-  page.$('button[type="submit"]').click();
-  // page.$('app-pages-layout').click();
+  })
 
-  Promise.all([
-    page.waitForNavigation(),
-    page.locator('label[for="mat-radio-3-input"]').click(),
-  ]);
-  Promise.all([
-    page.waitForNavigation(),
-    page.$('button[type="submit"]').click(),
-  ]);
-  page.waitForTimeout(3000);
-  page.fill('input[placeholder="Email"]', __ENV.second_username);
+
+  // Wait for the input field to appear and fill in username
+  const i1 = page.waitForSelector('input[id="username"]')
+  i1.type(__ENV.username)
+
+  // Wait for the Next button with its class name to appear
+  page.waitForSelector('.btn.btn-primary.btn-icon.justify-content-center')
+  page.locator('button[type="submit"]').click()
+
+  // Wait for the login container class on the next page
+  page.waitForSelector('.login-container')
+  // page.screenshot({ path: 'test/png/click_next.png'})
+
+  // Wait for the Radio Button to choose community login and click it
+  page.waitForSelector('.radio-controls.ng-star-inserted')
+  page.locator('label[for="mat-radio-3-input"]').click()
+  // Click Login button
+  page.locator('button[type="submit"]').click()
+
+  // Wait for the class that displays Email / Password input fields
+  // enter values
+  page.waitForSelector('.siteforceSldsOneColLayout.siteforceContentArea')
+  page.fill('input[placeholder="Email"]', __ENV.username);
   page.fill('input[placeholder="Password"]', __ENV.password);
+  // page.screenshot({ path: 'test/png/filled_second_username.png'})
+
+  // Wait for the Log In button to be displayed properly and click
+  page.waitForSelector('.sfdc')
+  page.locator('.slds-button.slds-button--neutral.sfdc_button.uiButton').click()
+  // page.screenshot({ path: 'test/png/click_login.png'})
+
+  // Wait for the main user landing page to be displayed
   Promise.all([
     page.waitForNavigation(),
-    page.$('button[type="button"]').click(),
-  ]);
-  Promise.all([
-    page.waitForNavigation(),
-    page.waitForTimeout(6000),
-  ]);
-  // console.warn(page.content());
+    page.waitForTimeout(3000),
+    page.waitForLoadState()
+  ])
+
+  // Click Warehouses link to display all warehouses
+  page.locator('a[href="/warehouse"]').click()
+  page.waitForNavigation()
+  // page.screenshot({ path: 'test/png/click_warehouses.png'})
+
+  // Check page content for a warehouse named DO-NOT-DELETE
   const body = page.content();
   check(body, {
-    'ContentHasKeyword': (body) => body.includes('Avalanche Summary'),
+    'ContentHasKeyword': (body) => body.includes('DO-NOT-DELETE'),
   });
-  //console.warn(page.content());
+ 
+  // Close browser orderly and exit
   page.close();
   browser.close();
 }
