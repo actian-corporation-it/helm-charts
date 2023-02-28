28f0b50109bbe1c79138697e4fc6df0259e676b7
Suleyman Kutlu <suleyman.kutlu@actian.com>
2023-02-28 14:46:30 +0000

    Updates:
    - Convert JScript code to async method in-sync with latest K6 code
    - Run 3 iterations for each test
    - Adjust gracefulstop same for both tests

--- charts/k6-synthetic-tests/synthetic-tests/avalanche.actiandatacloud.com.login.js
+++ charts/k6-synthetic-tests/synthetic-tests/avalanche.actiandatacloud.com.login.js
@@ -1,8 +1,11 @@
-import { chromium } from 'k6/x/browser';
+import { chromium, version } from 'k6/x/browser';
 import { check } from 'k6';
 import { response } from 'k6/x/browser';
 
 export const options = {
+  tags: {
+    k6cluster: __ENV.CLUSTER_NAME,
+  },
   thresholds: {},
   scenarios: {
     AvalancheLoginProduction: {
@@ -10,81 +13,109 @@ export const options = {
       executor: 'shared-iterations',
       gracefulStop: '60s',
       maxDuration: '10m',
-      vus: 5,
-      iterations: 5
+      vus: 3,
+      iterations: 3
     }
   }
 }
 
-export function avalanche_login_production() {
-  console.log('log')
-  console.debug('debug')
+export async function avalanche_login_production() {
   console.info('info')
-  console.warn('warn')
-  console.error('error')
 
   const browser = chromium.launch({
     args: ['start-maximized'],
     headless: true,
     slowMo: '500ms' // slow down by 500ms
   });
+
   const context = browser.newContext();
   const page = context.newPage();
 
-  // Goto main page, waittill page is loaded and check HTTP status code
-  const resp = page.goto('https://avalanche.actiandatacloud.com/')
-  page.waitForLoadState()
-  check(resp, {
-    'ResponseCodeIs200': (resp) => resp.status() == 200,
-  })
+  console.info('xk6-browser version: ', version);
+
+  try {
+    // Goto main page, waittill page is loaded and check HTTP status code
+    const resp = await page.goto('https://avalanche.actiandatacloud.com', { waitUntil: 'load'});
+    check(resp, {
+      'ResponseCodeIs200': (resp) => resp.status() == 200,
+    });
+
+    console.info("Goto page");
+
+    // Wait for the input field to appear and fill in username
+    // const i1 = await page.waitForSelector('input[id="username"]')
+    // i1.type(__ENV.username)
+    page.locator('input[id="username"]').type(__ENV.username);
+    console.info("Type username");
+
+    // Wait for the Next button with its class name to appear
+    //await Promise.all([
+    //  page.waitForNavigation(),
+    page.locator('button[type="submit"]').click();
+    //]);
+
+    console.info("Next pressed");
+
+    // Wait for the login container class on the next page
+    page.waitForSelector('.login-container')
+    // page.screenshot({ path: 'test/png/click_next.png'})
+
+    console.info("Login container")
+
+    // Wait for the Radio Button to choose community login and click it
+    page.waitForSelector('.radio-controls.ng-star-inserted')
+    await Promise.all([
+      page.waitForNavigation(),
+      page.locator('label[for="mat-radio-3-input"]').click(),
+      // Click Login button
+      page.locator('button[type="submit"]').click(),
+    ]);
+
+    console.info("Radio button 3 clicked and submitted")
+    // page.screenshot({ path: 'test/png/radio3_button_clicked.png'})
 
 
-  // Wait for the input field to appear and fill in username
-  const i1 = page.waitForSelector('input[id="username"]')
-  i1.type(__ENV.username)
+    // Wait for the class that displays Email / Password input fields
+    // enter values
+    page.locator('input[placeholder="Email"]').fill(__ENV.username);
+    page.locator('input[placeholder="Password"]').fill(__ENV.password);
+    // page.screenshot({ path: 'test/png/filled_second_username.png'})
 
-  // Wait for the Next button with its class name to appear
-  page.waitForSelector('.btn.btn-primary.btn-icon.justify-content-center')
-  page.locator('button[type="submit"]').click()
+    console.info("username password entered")
 
-  // Wait for the login container class on the next page
-  page.waitForSelector('.login-container')
+    // Wait for the Log In button to be displayed properly and click
+    await page.waitForSelector('.sfdc')
+    await page.locator('.slds-button.slds-button--neutral.sfdc_button.uiButton').click()
+    // page.screenshot({ path: 'test/png/click_login.png'})
 
-  // Wait for the Radio Button to choose community login and click it
-  page.waitForSelector('.radio-controls.ng-star-inserted')
-  page.locator('label[for="mat-radio-3-input"]').click()
-  // Click Login button
-  page.locator('button[type="submit"]').click()
+    console.info("Login clicked")
 
-  // Wait for the class that displays Email / Password input fields
-  // enter values
-  page.waitForSelector('.siteforceSldsOneColLayout.siteforceContentArea')
-  page.fill('input[placeholder="Email"]', __ENV.username);
-  page.fill('input[placeholder="Password"]', __ENV.password);
+    // Wait for the main user landing page to be displayed
+    Promise.all([
+      page.waitForNavigation(),
+      page.waitForTimeout(3000),
+      page.waitForLoadState()
+    ])
 
-  // Wait for the Log In button to be displayed properly and click
-  page.waitForSelector('.sfdc')
-  page.locator('.slds-button.slds-button--neutral.sfdc_button.uiButton').click()
+    console.info("Page loaded");
+    // page.screenshot({ path: 'test/png/page_loaded.png'})
 
-  // Wait for the main user landing page to be displayed
-  Promise.all([
-    page.waitForNavigation(),
-    page.waitForTimeout(3000),
-    page.waitForLoadState()
-  ])
+    // Click Warehouses link to display all warehouses
+    page.locator('a[href="/warehouse"]').click();
+    page.waitForNavigation();
+    // page.screenshot({ path: 'test/png/click_warehouses.png'})
 
-  // Click Warehouses link to display all warehouses
-  page.locator('a[href="/warehouse"]').click()
-  page.waitForNavigation()
+    console.info("Warehouse clicked")
 
-  // Check page content for a warehouse named DO-NOT-DELETE
-  const body = page.content();
-  check(body, {
-    'ContentHasKeyword': (body) => body.includes('DO-NOT-DELETE'),
-  });
- 
-  // Close browser orderly and exit
-  page.close();
-  browser.close();
+    // Check page content for a warehouse named DO-NOT-DELETE
+    const body = page.content();
+    check(body, {
+      'ContentHasKeyword': (body) => body.includes('DO-NOT-DELETE'),
+    });
+  } finally { 
+    // Close browser orderly and exit
+    page.close();
+    browser.close();
+  }
 }
 
